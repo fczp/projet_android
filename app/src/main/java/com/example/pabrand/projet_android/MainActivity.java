f73bd0eae438efbca7727704a9201dc9aa849daf
fczp <pierre.abrand@etu.u-bordeaux.fr>
2018-11-13 15:29:29 +0100

    Fonctionnement des Intent

--- app/src/main/java/com/example/pabrand/projet_android/MainActivity.java
+++ app/src/main/java/com/example/pabrand/projet_android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.pabrand.projet_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void consulter (View view) {
+        Intent intent = new Intent(this, Consulter.class);
+        startActivity(intent);
+    }
+
+    public void nouveau (View view) {
+        Intent intent = new Intent(this, Creer_rdv.class);
+        startActivity(intent);
+    }
+
 }

01a9a8146d39c772434955ea89152742cb062270
fczp <pierre.abrand@etu.u-bordeaux.fr>
2018-11-14 16:26:29 +0100

    Ajout des classes

---
+++ app/src/main/java/com/example/pabrand/projet_android/Contact.java
@@ -0,0 +1,23 @@
+package com.example.pabrand.projet_android;
+
+public class Contact {
+
+    private String nom;
+    private String numero;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
---
+++ app/src/main/java/com/example/pabrand/projet_android/RendezVous.java
@@ -0,0 +1,102 @@
+package com.example.pabrand.projet_android;
+
+import java.util.List;
+
+public class RendezVous {
+
+    static int numero = 1;
+
+    private int id;
+    private int annee;
+    private int mois;
+    private int jour;
+    private int heure;
+    private int minute;
+    private List<Contact> contacts;
+
+    public RendezVous(int id, int annee, int mois, int jour, int heure, int minute, List<Contact> contacts){
+        this.id = id;
+        this.annee = annee;
+        this.mois = mois;
+        this.jour = jour;
+        this.heure = heure;
+        this.minute = minute;
+        this.contacts = contacts;
+    }
+
+    public RendezVous(int annee, int mois, int jour, int heure, int minute, List<Contact> contacts){
+        this.id = numero ++;
+        this.annee = annee;
+        this.mois = mois;
+        this.jour = jour;
+        this.heure = heure;
+        this.minute = minute;
+        this.contacts = contacts;
+    }
+
+    public void ajouterContact(Contact contact){
+        List<Contact> contacts_temp = this.getContacts();
+        contacts_temp.add(contact);
+        this.setContacts(contacts_temp);
+    }
+
+    public static int getNumero() {
+        return numero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    public int getMois() {
+        return mois;
+    }
+
+    public void setMois(int mois) {
+        this.mois = mois;
+    }
+
+    public int getJour() {
+        return jour;
+    }
+
+    public void setJour(int jour) {
+        this.jour = jour;
+    }
+
+    public int getHeure() {
+        return heure;
+    }
+
+    public void setHeure(int heure) {
+        this.heure = heure;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+}

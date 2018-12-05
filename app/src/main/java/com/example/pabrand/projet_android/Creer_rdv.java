15e02993cba665e625316c2c7e00ca1fa06cbbc3
Hjelad <gaetan.dore@gmail.com>
2018-12-05 01:24:54 +0100

    recup numero tel

--- app/src/main/java/com/example/pabrand/projet_android/Creer_rdv.java
+++ app/src/main/java/com/example/pabrand/projet_android/Creer_rdv.java
@@ -1,9 +1,13 @@
 package com.example.pabrand.projet_android;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,12 +16,13 @@ import android.view.View;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class Creer_rdv extends AppCompatActivity {
 
-    private static final int PICK_CONTACT = 0;
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
     private TextView affDate;
     private DatePickerDialog.OnDateSetListener dateListener;
 
@@ -27,9 +32,42 @@ public class Creer_rdv extends AppCompatActivity {
         setContentView(R.layout.activity_creer_rdv);
     }
 
-    public void contact (View view){
-        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(intent, PICK_CONTACT);
+    public void pickContact(View view) {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
+        // Check which request we're responding to
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+
+                // Get the URI that points to the selected contact
+                Uri contactUri = resultIntent.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using <code><a href="/reference/android/content/CursorLoader.html">CursorLoader</a></code> to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+
+                Toast toast = Toast.makeText(getApplicationContext(), number, Toast.LENGTH_SHORT);
+                toast.show();
+                // Do something with the contact here (bigger example below)
+            }
+        }
     }
 
     public void choisirDate(View view){
